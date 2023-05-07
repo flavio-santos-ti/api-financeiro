1b289abfe1f724ea7713871df7c8e72bce70ef85
Flávio dos Santos <flavio.santos.ti@outlook.com>
2023-05-07 15:30:44 -0300

    O método CategoriaService.GetAsync() foi testado.

--- Backend.Financeiro/Test.API.Financeiro/Services/CategoriaServiceTest.cs
+++ Backend.Financeiro/Test.API.Financeiro/Services/CategoriaServiceTest.cs
@@ -2,6 +2,7 @@ using Api.Crud.Infra.Data.Interfaces;
 using API.Financeiro.Business.Mappers;
 using API.Financeiro.Business.Services;
 using API.Financeiro.Business.Validators.Categoria;
+using API.Financeiro.Domain.Categoria;
 using API.Financeiro.Domain.Result;
 using API.Financeiro.Infra.Data.Interfaces;
 using AutoMapper;
@@ -26,7 +27,7 @@ public class CategoriaServiceTest
     }
 
     [TestMethod]
-    [TestCategory("Categoria - Service")]
+    [TestCategory("Método - GetViewAllAsync()")]
     [DataRow(0, 2)]
     public async Task Se_o_Skip_for_igual_a_Zero_retorna_resultado_igual_a_False(int skip, int take)
     {
@@ -44,9 +45,9 @@ public class CategoriaServiceTest
 
 
     [TestMethod]
-    [TestCategory("Categoria - Service")]
+    [TestCategory("Método - GetViewAllAsync()")]
     [DataRow(3, 2)]
-    public async Task Se_o_Skip_for_igual_a_Tres_retorna_resultado_igual_a_False(int skip, int take)
+    public async Task Se_o_Skip_for_igual_a_Tres_retorna_resultado_igual_a_True(int skip, int take)
     {
         // Arrange
         CreateCategoriaValidator validator = new CreateCategoriaValidator();
@@ -55,6 +56,42 @@ public class CategoriaServiceTest
         // Act
         ServiceResult retorno = await categoria.GetViewAllAsync(skip, take);
         bool resultado = retorno.Count == 0;
+
+        // Assert
+        Assert.IsTrue(resultado);
+    }
+
+    [TestMethod]
+    [TestCategory("Método - GetAsync()")]
+    [DataRow(1)]
+    public async Task Se_o_Id_for_igual_a_Um_retorna_o_resultado_igual_a_True(long id)
+    {
+        // Arrange
+        CreateCategoriaValidator validator = new CreateCategoriaValidator();
+        var categoria = new CategoriaService(validator, _unitOfWork, _categoriaRepository, _mapper);
+
+        // Act
+        Categoria retorno = await categoria.GetAsync(id);
+
+        bool resultado = retorno != null;
+
+        // Assert
+        Assert.IsTrue(resultado);
+    }
+
+    [TestMethod]
+    [TestCategory("Método - GetAsync()")]
+    [DataRow(2)]
+    public async Task Se_o_Id_for_igual_a_Dois_retorna_o_resultado_igual_a_True(long id)
+    {
+        // Arrange
+        CreateCategoriaValidator validator = new CreateCategoriaValidator();
+        var categoria = new CategoriaService(validator, _unitOfWork, _categoriaRepository, _mapper);
+
+        // Act
+        Categoria retorno = await categoria.GetAsync(id);
+
+        bool resultado = retorno == null;
 
         // Assert
         Assert.IsTrue(resultado);
