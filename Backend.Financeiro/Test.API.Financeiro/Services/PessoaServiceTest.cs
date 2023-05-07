aabcc36239c0e85d8624c2f4f4f4a884f8fe61d6
Flávio dos Santos <flavio.santos.ti@outlook.com>
2023-05-07 19:44:31 -0300

    Foi implementado os testes na classe PessoaService.

---
+++ Backend.Financeiro/Test.API.Financeiro/Services/PessoaServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Api.Crud.Infra.Data.Interfaces;
+using API.Financeiro.Business.Services;
+using API.Financeiro.Business.Validators.Cliente;
+using API.Financeiro.Domain.Cliente;
+using API.Financeiro.Domain.Pessoa;
+using API.Financeiro.Domain.Result;
+using API.Financeiro.Infra.Data.Interfaces;
+using API.Financeiro.Infra.Data.Repositories;
+using Test.API.Financeiro.Repository;
+using Test.API.Financeiro.UnitOfWork;
+
+namespace Test.API.Financeiro.Services;
+
+[TestClass]
+public class PessoaServiceTest
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IPessoaRepository _pessoaRepository;
+
+
+    public PessoaServiceTest()
+    {
+        _unitOfWork = new UnitOfWorkFake();
+        _pessoaRepository = new PessoaRepositoryFake();
+    }
+
+    [TestMethod]
+    [TestCategory("Método - CreateAsync()")]
+    [DataRow("Dina")]
+    public async Task CreateAsync_Se_o_Nome_Ja_Existir_retorna_o_resultado_igual_a_False(string nome)
+    {
+        // Arrange
+        var pessoa = new PessoaService(_unitOfWork, _pessoaRepository);
+
+        // Act
+        Pessoa retorno = await pessoa.CreateAsync(nome);
+
+        bool resultado = retorno == null;
+
+        // Assert
+        Assert.IsFalse(resultado);
+    }
+
+
+    [TestMethod]
+    [TestCategory("Método - CreateAsync()")]
+    [DataRow("Roberto")]
+    public async Task CreateAsync_Se_o_Nome_Nao_Existir_retorna_o_resultado_igual_a_True(string nome)
+    {
+        // Arrange
+        var pessoa = new PessoaService(_unitOfWork, _pessoaRepository);
+
+        // Act
+        Pessoa retorno = await pessoa.CreateAsync(nome);
+
+        bool resultado = retorno != null;
+
+        // Assert
+        Assert.IsTrue(resultado);
+    }
+}
